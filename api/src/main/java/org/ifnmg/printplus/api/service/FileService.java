326bcf0fcdd4c6935ab030df37951bfad372d156
rodriguesxxx <rodriguesx.daniel89@gmail.com>
2024-03-03 15:40:40 -0300

    [REFACTOR] removing abstraction from the model File

--- api/src/main/java/org/ifnmg/printplus/api/service/FileService.java
+++ api/src/main/java/org/ifnmg/printplus/api/service/FileService.java
@@ -1,41 +1,43 @@
 package org.ifnmg.printplus.api.service;
 
 import java.io.IOException;
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 
-import org.ifnmg.printplus.api.model.File;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+//TODO: refatorar código: remover model File.
+//TODO: converter arquivo para pdf antes de salvar
 @Service
 public class FileService {
     
-    private File file;
+    private MultipartFile file;
 
     @Value("${storage.path}")
     private String path;
 
     public FileService() {}
 
-    public FileService(File file) {
+    public FileService(MultipartFile file) {
         this.file = file;
     }
 
-    public void setFile(File file) {
+    public void setFile(MultipartFile file) {
         this.file = file;
     }
 
     public void save(String sessionId) throws IOException {
 
-        java.io.File newPath = new java.io.File(path + sessionId);
+        File newPath = new File(path + sessionId);
         if(!newPath.exists()) {
             newPath.mkdirs();
         }
 
-        String finalPath = newPath.getAbsolutePath() + "/" + file.getFilename();
+        String finalPath = newPath.getAbsolutePath() + "/" + file.getOriginalFilename();
         Files.copy(file.getInputStream(), Path.of(finalPath), StandardCopyOption.REPLACE_EXISTING);
-
     }
 }
